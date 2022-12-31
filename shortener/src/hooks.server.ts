8b0d82db8d72ac8d87c9e735af20bf52ffc9276f
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-12-30 19:02:03 -0800

    add approved admins

--- shortener/src/hooks.server.ts
+++ shortener/src/hooks.server.ts
@@ -2,12 +2,19 @@ import { dev } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_AUTH_ID, GITHUB_AUTH_SECRET } from "$env/static/private"
+import { GITHUB_AUTH_ID, GITHUB_AUTH_SECRET, APPROVED_ADMINS } from "$env/static/private"
+
+const approvedAdmins = APPROVED_ADMINS.trim().split(/\s*,\s*/);
 
 const authHandler = SvelteKitAuth({
 	providers: [
 		GitHub({ clientId: GITHUB_AUTH_ID, clientSecret: GITHUB_AUTH_SECRET }) as any,
-	]
+	],
+	callbacks: {
+		signIn({ user }) {
+			return approvedAdmins.includes(user.name ?? '')
+		}
+	}
 });
 
 export const handle: Handle = async ({ event, resolve }) => {
