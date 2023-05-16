5c239a7771c8d3f64e6d6aa807df3b37568d84d9
EmNudge <24513691+EmNudge@users.noreply.github.com>
2023-05-16 10:05:42 -0700

    auto select input and allow new link key (#3)

--- shortener/src/components/Search.svelte
+++ shortener/src/components/Search.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
+	import { showModal } from "$lib/modal";
+	import { onMount } from "svelte";
+
 	export let value: string = '';
+
+	let searchEl: HTMLInputElement;
+	onMount(() => {
+		const handleType = (e: KeyboardEvent) => {
+			if (document.activeElement instanceof HTMLInputElement || document.activeElement instanceof HTMLTextAreaElement) return;
+			if (e.key === 'N') {
+				showModal({ type: 'create', defaultVisibility: 'public' });
+				e.preventDefault();
+				return;
+			}
+			if (/^[a-zA-Z]$/.test(e.key)) {
+				searchEl.focus();
+				return;
+			}
+		};
+		document.addEventListener('keydown', handleType);
+	})
 </script>
 
 <label>
-	<input type="text" bind:value placeholder="Search..." />
+	<input type="text" bind:value placeholder="Search..." bind:this={searchEl} />
 	<img src="/search.svg" alt="search" />
 </label>
 
